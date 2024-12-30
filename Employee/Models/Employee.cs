5530c40d93152b475c95d27a077c75bfca9765a4
Adebayo Awwal <adebayoawwaldele@gmail.com>
2024-12-30 19:59:59 +0100

    first commit

---
+++ Employee/Models/Employee.cs
@@ -0,0 +1,30 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Employee.Models
+{
+    public class Employee
+    {
+        [Key]
+        public Guid ID { get; set; }
+        [Required]
+        public string FirstName { get; set; }
+        [Required]
+        public string MiddleName { get; set; }
+        [Required]
+        public string LastName { get; set; }
+        [Required]
+        public string FullName { get; set; }
+        [Required] 
+        public string Address { get; set; }
+        [Required]
+        public string Email { get; set; }
+        [Required]
+        public int NextOfCin { get; set; }
+        [Required]
+        public int PhoneNumber { get; set; }
+        [Required]
+        public int Password { get; set; }
+        [Required]
+        public string Gender { get; set; }
+   }
+}
